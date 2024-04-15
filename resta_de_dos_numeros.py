92d8cfcbe056a6966c5e6405a4b86f856887112d
Facundo <frm.code@gmail.com>
2024-04-15 16:05:58 -0300

    algoritmo basico de resta

---
+++ resta_de_dos_numeros.py
@@ -0,0 +1,11 @@
+nombre = input("¿Cual es tu nombre? :")
+
+primer_numero = int(
+    input("Hola, " + nombre + " haremos una resta, introduce el primer numero : "))
+
+segundo_numero = int(input("Introduce el segundo numero : "))
+
+resultado = primer_numero - segundo_numero
+
+print("El resultado es ", primer_numero,
+      " - ", segundo_numero, " = ", resultado)
